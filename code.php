3c6dcacd820c0584383456b10f9b5cef5332423a
Saksham Sharma <saksham.sh17@gmail.com>
2018-12-25 22:29:50 +0530

    Update code.php

--- code.php
+++ code.php
@@ -8,3 +8,10 @@ $host='localhost';
       }catch(PDOException $e){ 
            echo"Not Connected..".$e->getMessage(); 
       } 
+      // Get Ip
+       $ip = $_SERVER['REMOTE_ADDR'];
+       // Check if this ip exist in out data 
+       $sql="SELECT ip FROM visitor WHERE ip='$ip'";
+       $Check=$DBH ->prepare($sql); 
+       $Check->execute();
+       $CheckIp=$Check->rowCount();
